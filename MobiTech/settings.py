b1fcb5865ea8d124ca37a050bb84b4a577989eb6
irfaxs <irfanippu291@gmail.com>
2026-07-02 21:10:13 +0530

    deployment

--- MobiTech/settings.py
+++ MobiTech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+14ep)3a=w!jl=fdqc(xjo(t8sr2&lucd5j)^n(mvsg3m8g%ri
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,9 +128,10 @@ STATIC_URL = 'static/'
 
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static' 
 ]
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
  
 MEDIA_ROOT =   BASE_DIR/'media'   
 MEDIA_URL = '/'
